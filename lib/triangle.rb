1dd1bbcf4731591900025df2b4409a0095a76c9c
github username <github email address>
2021-03-02 11:33:38 -0500

    commit tree bd7f2aaef6c7433dbec240e1efb281f1b3c41915

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,27 +9,28 @@ class Triangle
   end
 
   def kind
-    if @length_1 <=0 || @length_2 <=0 || @length_3
-      begin
+    if @length_1 <=0 || @length_2 <=0 || @length_3 <=0 
         raise TriangleError
-      rescue TriangleError => error
-          puts error.message
-      end
+      # rescue TriangleError => error #the goal is for our test to pass, and having the rescue in the code will never make it pass.
+          # puts error.message #the tests are looking for an error which the rescue prevents. 
+      elsif @length_1 + @length_2 < @length_3 || @length_1 + @length_3 < @length_2 || @length_2 + @length_3 < @length_1
+        raise TriangleError
+      elsif @length_1 + @length_2 == @length_3 || @length_1 + @length_3 == @length_2 || @length_2 + @length_3 == @length_1
+        raise TriangleError
       elsif @length_1 == @length_2 && @length_2 == @length_3
-      @equilateral
-      
-    elsif @length_1 == @length_2 || @length_2 == @length_3
-      @isosceles
+      :equilateral
+      elsif @length_1 == @length_2 || @length_2 == @length_3 || @length_1 == @length_3 
+      :isosceles
     else
-      @scalene
+      :scalene
     end
   end
 
 
   class TriangleError < StandardError
-    def message
-      "testing this error message"
-    end
+    # def message
+    #   "testing this error message"
+    # end
   end
 
 end
