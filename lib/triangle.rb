914e9a247ddc03eba757ebef8c3a9df32146bb9e
github username <github email address>
2021-03-02 11:02:46 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+ 
+  def initialize(length_1, length_2,length_3)
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
+  end
+
+  def kind
+    if @length_1 <=0 || @length_2 <=0 || @length_3
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end
+      elsif @length_1 == @length_2 && @length_2 == @length_3
+      @equilateral
+      
+    elsif @length_1 == @length_2 || @length_2 == @length_3
+      @isosceles
+    else
+      @scalene
+    end
+  end
+
+
+  class TriangleError < StandardError
+    def message
+      "testing this error message"
+    end
+  end
+
 end
